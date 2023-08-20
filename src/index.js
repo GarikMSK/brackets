d726109701dd74da93eaabdb408c7779d0da5a66
GarikMSK <igor@nbma.ru>
2023-08-20 21:29:54 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+/*
 module.exports = function check(str, bracketsConfig) {
   // your solution
 }
+*/
+
+const bracketsConfig = "[['(', ')']]";
+let bracketsConfig2 = [1, 2, 3]
+const stack = [];
+for (const bracket of bracketsConfig) 
+  {
+    if (bracket === '(') {
+    stack.push(bracket);
+    }
+    else if (bracket === ')') {
+      if (!stack.pop()) {
+        return false;
+      }
+    }
+    console.log(stack);
+  }
