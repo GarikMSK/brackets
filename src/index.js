aa9d15ebd02834e9b519b44cd1b360676ed8e977
GarikMSK <igor@nbma.ru>
2023-08-21 00:27:47 +0300

    init: ready

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
-/*
+
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
-*/
 
-const bracketsConfig = "[['(', ')']]";
-let bracketsConfig2 = [1, 2, 3]
+
+result = true;
+const bracketsConfig = "|()|";
 const stack = [];
+let lineCounter = 0;
 for (const bracket of bracketsConfig) 
   {
-    if (bracket === '(') {
+    if (bracket === "(" || bracket === "[" || bracket === "{" || (bracket === "|" && (lineCounter == 0))) {
+    if (bracket === "|") {lineCounter++}
     stack.push(bracket);
     }
     else if (bracket === ')') {
-      if (!stack.pop()) {
-        return false;
-      }
+      
+      let stackElement = stack.pop();
+      console.log(stackElement);
+      if('(' != stackElement) 
+        {console.log("false");result = false;}
     }
+    else if (bracket === ']') {
+      let stackElement = stack.pop();
+      console.log(stackElement);
+      if('[' != stackElement) 
+        {console.log("false");result = false;}
+     }
+     else if (bracket === '}') {
+      let stackElement = stack.pop();
+      console.log(stackElement);
+      if('{' != stackElement) 
+        {console.log("false");result = false;}
+     }
+     else if (bracket === "|" && (lineCounter == 1)) {
+      let stackElement = stack.pop();
+      console.log(stackElement);
+      if('|' != stackElement) 
+        {console.log("false");result = false};
+        lineCounter = 0;
+     }
+     
     console.log(stack);
   }
+  console.log(stack.length === 0);
+  if(stack.length === 0){result = true} else {result = false}; console.log("and total ",result);
+return result;
+}
